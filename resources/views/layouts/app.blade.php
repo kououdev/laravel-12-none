cb1fe0fb30f94ae29e7263b33008b98a7a861361
kououdev <kououdev@gmail.com>
2025-09-26 17:24:37 +0700

    update layout done basic

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,28 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Dashboard' }}</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors" />
+    <meta name="generator" content="Hugo 0.84.0" />
+    <title>Dashboard Template · Bootstrap v5.0</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
     @livewireStyles
 </head>
 
@@ -16,14 +34,14 @@
     <div class="container-fluid">
         <div class="row">
             {{-- Sidebar --}}
-            <div class="col-2">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
                 @include('partials.sidebar')
-            </div>
+            </nav>
 
             {{-- Content --}}
-            <div class="col-10">
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 {{ $slot }}
-            </div>
+            </main>
         </div>
     </div>
 
