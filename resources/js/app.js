29e37faffedd3ab86142f9e75c711103faccd845
kououdev <kououdev@gmail.com>
2025-09-26 10:23:35 +0700

    init

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+// 1. Laravel bootstrap (axios, echo, dll)
+import "./bootstrap";
+
+// 2. CSS global
+import "../css/app.css";
+
+// 3. JS Library tambahan (bootstrap, alpine, dll)
+import "bootstrap/dist/js/bootstrap.bundle";
