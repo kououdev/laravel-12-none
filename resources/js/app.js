409eb3ed22340eea8fb99e9cb9ec8f0dab05e244
kououdev <kououdev@gmail.com>
2025-09-26 10:55:11 +0700

    add templete

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import "./bootstrap";
 
 // 2. CSS global
 import "../css/app.css";
+import "../css/dashboard.css";
 
 // 3. JS Library tambahan (bootstrap, alpine, dll)
 import "bootstrap/dist/js/bootstrap.bundle";
+import "../js/dashboard";
